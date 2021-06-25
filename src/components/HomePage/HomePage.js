26befee6e08b2249ce4e7ebd1bc34fcb14d12461
Ashlyn Smith <ashlyn.marie.smith2@gmail.com>
2021-06-25 14:48:20 -0400

    started homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,23 +1,29 @@
-import React from "react";
-import { Heading, Container } from "@chakra-ui/react";
-
-
+import React from 'react';
+import { Center, Box, Container } from '@chakra-ui/react';
 
 const HomePage = () => {
-
-
-    return(
-        <>
-
-<Heading>Stream</Heading>
-        <Container >
-  There are many benefits to a joint design and development system. Not only
-  does it bring benefits to the design team, but it also brings benefits to
-  engineering teams. It makes sure that our experiences have a consistent look
-  and feel, not just in our design specs, but in production
-</Container>
-    
+  return (
+    <>
+      <Center>
+        <Box
+          bgColor="#F8FCFD"
+          w={1000}
+          h={60}
+          border="1px"
+          borderColor="#707070"
+        >
+          <Center>
+            <Container m={10}>
+              There are many benefits to a joint design and development system.
+              Not only does it bring benefits to the design team, but it also
+              brings benefits to engineering teams. It makes sure that our
+              experiences have a consistent look and feel, not just in our
+              design specs, but in production
+            </Container>
+          </Center>
+        </Box>
+      </Center>
     </>
-    )
+  );
 };
 export default HomePage;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from 'components/HomePage/HomePage';
+import Stream from 'components/HomePage/Stream';
+
+const Home = () => {
+  return (
+    <>
+      <Stream />
+      <HomePage />
+    </>
+  );
+};
+
+export default Home;
